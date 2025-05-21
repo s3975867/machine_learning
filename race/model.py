b1f5f6832e6a2a4adf5be43109921fa6d82e1f0a
s3971187 <s3971187@student.rmit.edu.au>
2025-05-21 12:38:04 +1000

    Implement Unlearning

--- race/model.py
+++ race/model.py
@@ -4,22 +4,13 @@ from torchvision.models import resnet18
 class AgeClassifier(nn.Module):
     def __init__(self, num_classes=5, pretrained=True):
         super(AgeClassifier, self).__init__()
-        self.cnn = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=7, padding=3),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(8, 16, kernel_size=5, padding=2),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(16, 3, kernel_size=3, padding=1),
-            nn.AdaptiveAvgPool2d((224, 224))
-        )
-        self.backbone = resnet18(pretrained=pretrained)
+        self.base = resnet18(pretrained=pretrained)
         
-        num_features = self.backbone.fc.in_features
-        self.backbone.fc = nn.Sequential(
+        num_features = self.base.fc.in_features
+        self.base.fc = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(num_features, num_classes),
         )
         
     def forward(self, x):
-        x = self.cnn(x)
-        return self.backbone(x)
+        return self.base(x)
